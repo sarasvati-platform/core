a42376c18eedd08396b780f8655e05a0a702a505
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-06-26 11:55:37 +0000

    feat: add exports

--- src/core/commands/index.ts
+++ src/core/commands/index.ts
@@ -1,23 +1,2 @@
 export { ICommand } from './command'
 export { Processor } from './processor'
-
-
-// class Identity<TType, Brand> {
-//   constructor(private readonly value: TType) { }
-
-//   equals(other: Identity<TType, Brand>): boolean {
-//     return this.value === other.value
-//   }
-//   private __brand__: Brand // вся магия тут
-// }
-
-
-// class TaskId extends Identity<number, 'TaskId'> { }
-// class ProjectId extends Identity<number, 'ProjectId'> { }
-
-// function assignTaskToProject(taskId: TaskId, projectId: ProjectId) {}
-
-// const taskId = new TaskId(42) as ProjectId
-// const wtfId = new Identity<number>(42) as ProjectId as TaskId
-
-// assignTaskToProject(wtfId, taskId)
---
+++ src/core/persistence/index.ts
@@ -0,0 +1,6 @@
+export {
+  Query,
+  and, or, not,
+  eq, neq, gt, lt, gte, lte, incl
+} from './query'
+export { IRepository } from './repository'
